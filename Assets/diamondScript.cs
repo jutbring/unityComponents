d8ab3054c61f525be395177763beed4ba4ec6974
LEARNET\william.jutbring <william.jutbring@elev.ga.lbs.se>
2018-10-02 10:48:23 +0200

    bilen blev

---
+++ Assets/diamondScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class diamondScript : MonoBehaviour
+{
+    public SpriteRenderer rend;
+    public Transform other;
+    public Color newColor;
+
+    [Range(-720, 720)]
+    public float rotationSpeed; 
+    // Use this for initialization
+    void Start()
+    {
+
+        rend.color = newColor;
+        //other.localPosition = new Vector3(3, 8, other.position.z);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(0f, 0f, -rotationSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0.2f, 0, 0);
+        }
+    }
+}
